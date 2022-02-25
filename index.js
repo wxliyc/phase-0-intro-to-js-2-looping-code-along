85c799d4468b372e615a89b2ae613bf8d07ceed6
Ethan Lee <wxliyc@gmail.com>
2022-02-25 10:16:57 -0600

    commit tree ff7fa593beea7df4a426ac590428f5830e0e5770

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Charlie", "Samip", "Ali"];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]} for the wonderful birthday gift!`);
+    }
+  return cards;
+}
+
+writeCards(cards);
+
+let i = 10;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
